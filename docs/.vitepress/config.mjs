8b3295d6b9ac86b31ed985302ed87770782673b9
coderzry <18348738308@163.com>
2024-08-28 11:07:34 +0800

    fix: edit themeConfig

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -20,31 +20,40 @@ export default defineConfig({
     nav: [
       { text: '首页', link: '/' },
       { text: '技术记录', link: '/skill/内网开发提效指南' },
-      { text: '年度总结', link: '/summary/2023年度总结' }
+      { text: '年度总结', link: '/summary/2023年度总结' },
+      { text: 'vue技巧', link: '/vue-tips/' }
     ],
 
-    sidebar: [
-      {
-        text: '技术记录',
-        items: [
-          { text: '内网开发提效指南', link: '/skill/内网开发提效指南' },
-          { text: '富文本编辑器改造', link: '/skill/富文本编辑器改造' }
-        ]
-      },
-      {
-        text: '年度总结',
-        items: [
-          { text: '年度总结', link: '/summary/2023年度总结' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/skill/': [
+        {
+          text: '技术记录',
+          items: [
+            { text: '内网开发提效指南', link: '/skill/内网开发提效指南' },
+            { text: '富文本编辑器改造', link: '/skill/富文本编辑器改造' }
+          ]
+        }
+      ],
+      '/summary/': [
+        {
+          text: '年度总结',
+          items: [
+            { text: '2023年度总结', link: '/summary/2023年度总结' }
+          ]
+        }
+      ]
+    },
 
     search: {
       provider: 'local'
     },
 
+    lastUpdated: {
+      text: 'Updated at'
+    },
+
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/coderzry' }
     ]
   }
 })
